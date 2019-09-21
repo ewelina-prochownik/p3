28bc809bd95990a2a5bd12b945242c40e8dee552
Ewelina Prochownik <ewe.prochownik@gmail.com>
2019-09-21 14:33:18 +0200

    Add class Hello

---
+++ zajecia_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace zajecia_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello.WriteHello();
+            Console.Read();
+        }
+    }
+}
