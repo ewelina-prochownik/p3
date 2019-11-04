c718f85295d8dd00c732e2c434acdd4bd540f3fa
ewelina-prochownik <ewe.prochownik@gmail.com>
2019-11-04 21:57:40 +0100

    Create new class and function

---
+++ zad01/zad01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WritingClass.PrintFunction();
+        }
+    }
+}
---
+++ zad01/zad01/WritingClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zad01
+{
+    public class WritingClass
+    {
+        public static void PrintFunction()
+        {
+            Console.WriteLine("It's first function.");
+        }
+    }
+}
