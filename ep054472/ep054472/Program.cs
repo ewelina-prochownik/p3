232c6b6a7ef187a74027eab9c46436c29ccf8ee1
ewelina-prochownik <ewe.prochownik@gmail.com>
2020-01-23 21:52:58 +0100

    Added lab07, renamed old lab07

---
+++ ep054472/ep054472/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ep054472
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
