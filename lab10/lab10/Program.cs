56e06df0feb44219fbd112bb5edaf269d086d042
ewelina-prochownik <ewe.prochownik@gmail.com>
2020-01-25 19:51:45 +0100

    Program with external data

---
+++ lab10/lab10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("data.txt");
+            List<Person> people = lines.Select(x =>
+            {
+                var data = x.Split(',');
+                return new Person(
+                    Convert.ToInt32(data[0]),
+                    data[1],
+                    data[2],
+                    data[3]);
+            }).ToList();
+
+            var sortedPeople = people
+                .OrderBy(x => x.LastName)
+                .ThenBy(x => x.Name);
+            
+            using StreamWriter writer = new StreamWriter("result.txt");
+            foreach (var item in sortedPeople)
+            {
+                writer.WriteLine($"[{item.Id}] {item.Name} {item.LastName} {item.Phone}");
+            }
+        }
+    }
+}
